ae065ffd0b5b3329ad49321acddd3b6f1c71ddb7
Khang <khangdd6936@ut.edu.vn>
2025-06-22 21:22:10 +0700

    config structure

---
+++ index.js
@@ -0,0 +1,9 @@
+async function loadHTML(id, file) {
+    const response = await fetch(file);
+    const html = await response.text();
+    document.getElementById(id).innerHTML = html;
+}
+
+loadHTML("header", "./layout/header/header.html");
+// loadHTML("content", "content.html");
+loadHTML("footer", "./layout/footer/footer.html");
